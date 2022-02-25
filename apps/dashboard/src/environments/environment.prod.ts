f7e312d03cc8d4d6900179b40ca68cbe56f3c624
Amin Nateghi <amin.programmer@gmail.com>
2022-02-25 17:08:06 +0300

    added api to environment

--- apps/dashboard/src/environments/environment.prod.ts
+++ apps/dashboard/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from '../app/shared/models/environment';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'http://localhost:3333',
+  apiUrl: 'http://localhost:3333/api',
 };
--- apps/dashboard/src/environments/environment.ts
+++ apps/dashboard/src/environments/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from "../app/shared/models/environment";
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:3333',
+  apiUrl: 'http://localhost:3333/api',
 };
