3dc6f8c1bf569d921a2f606c98ee43f0f5df9d6a
Amin Nateghi <amin.programmer@gmail.com>
2022-02-26 01:34:37 +0300

    test deploy in Heroku

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { HealthModule } from './controllers/health/health.module';
 import { ReportModule } from './controllers/report/report.module';
@@ -10,7 +12,11 @@ import { ReportModule } from './controllers/report/report.module';
     HealthModule,
     ReportModule,
     HttpModule,
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'dashboard'),
+      exclude: ['/api*']
+    })
   ]
 })
 export class AppModule { }
